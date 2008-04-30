9db2437dc8901c4933ab12bd16f8796556137dc3
Marcel Haerry <haerry@puzzle.ch>
2008-04-30 10:41:03 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# hal module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "hal": }
+class hal {}
