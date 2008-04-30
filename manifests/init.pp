49426ccc2f3795540f85fd5d166aee972f9a5b8a
Marcel Haerry <haerry@puzzle.ch>
2008-04-30 10:51:16 +0200

    added haldaemon and disable

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,19 @@
 
 
 # modules_dir { "hal": }
-class hal {}
+class hal {
+    include hal::base
+}
+
+class hal::base {
+    package{hal:
+        require => present,
+    }
+
+    service{:haldaemon:
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+        require => Package[hal],
+    } 
+}
